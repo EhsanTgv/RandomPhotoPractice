d3621eaa73920553514316fab8fe64762becafa3
EhsanTgv <EhsanTgv@gmail.com>
2023-01-20 09:28:01 +0300

    Initial Commit

---
+++ RandomPhotoPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhotoPractice
+//
+//  Created by Ehsan Taghavi on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
